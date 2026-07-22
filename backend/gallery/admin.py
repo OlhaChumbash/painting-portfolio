4b64d54c565361fe6eed8165444a489c173aae0d
OlhaChumbash <123654087+OlhaChumbash@users.noreply.github.com>
2026-07-22 19:06:14 +0000

    Add gallery painting model

--- backend/gallery/admin.py
+++ backend/gallery/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Painting
 
-# Register your models here.
+
+@admin.register(Painting)
+class PaintingAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "created_at",
+        "updated_at",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+    )
