ef94e4a8e74f7ceb3dbf80b1c838f67e48e50e57
unknown <olyachubash@gmail.com>
2026-08-28 19:49:19 +0200

    feature/transition_effect

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,6 +2,8 @@ import '@/styles/globals.css'
 import { Montserrat } from "next/font/google"
 import NavBar from "@/components/NavBar"
 import Footer from "@/components/Footer"
+import { AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
 
 
 const montserrat = Montserrat({
@@ -10,10 +12,13 @@ const montserrat = Montserrat({
 })
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark  w-full min-h-screen`}>
       <NavBar />
-      <Component {...pageProps} />
+      <AnimatePresence mode='wait' >
+        <Component key={router.asPath} {...pageProps} />
+      </AnimatePresence>
       <Footer />
     </main>
   )
